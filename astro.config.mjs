2a81b7cc53f91e98b8fec132d84a6901acf20822
MichalRsa <michalrosa17@gmail.com>
2024-02-01 13:52:44 +0100

    Add a headless CMS to Astro in 5 minutes

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+import { loadEnv } from "vite";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
